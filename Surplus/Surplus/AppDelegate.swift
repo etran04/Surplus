9e281a70c7fbfa0917090bf0714b213facd27f85
Ryan Lee <ryanyunglee@gmail.com>
2016-01-31 16:43:23 -0800

    import firebase

--- Surplus/Surplus/AppDelegate.swift
+++ Surplus/Surplus/AppDelegate.swift
@@ -19,9 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        ref.setValue("Do you have data? You'll love Firebase.")
-        
         return true
     }
 

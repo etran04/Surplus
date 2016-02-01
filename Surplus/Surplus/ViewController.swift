e56f2a5c42a98443028edd10c5c52afc820c0cd0
Ryan Lee <ryanyunglee@gmail.com>
2016-01-31 18:31:36 -0800

    saves id and username at login

--- Surplus/Surplus/ViewController.swift
+++ Surplus/Surplus/ViewController.swift
@@ -11,7 +11,7 @@ import FBSDKLoginKit
 import Firebase
 import FBSDKShareKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         let facebookButton = FBSDKLoginButton()
         facebookButton.center = self.view.center
         self.view.addSubview(facebookButton)
+        facebookButton.readPermissions = ["public_profile", "email", "user_friends"]
+        facebookButton.delegate = self
         
         // Create a UIButton styled for sharing to messenger. You can leave
         // the size at its default (365, 45) or change the size yourself
@@ -33,6 +35,67 @@ class ViewController: UIViewController {
     func shareButtonPressed() {
         print("share putton pressed")
         FBSDKMessengerSharer.openMessenger()
+    }
+    
+    func saveUserInfo() {
+        let ref = Firebase(url: "https://calpolysurplus.firebaseio.com")
+        let facebookLogin = FBSDKLoginManager()
+        
+        //facebookLogin.
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                //saveUserInfo()
+                returnUserData()
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
+    
+    func returnUserData()
+    {
+        let ref = Firebase(url: "https://calpolysurplus.firebaseio.com")
+
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                
+                let id = result.valueForKey("id") as! NSString
+                let usersRef = ref.childByAppendingPath("users/\(id)")
+                let newUser = ["username" : "\(userName)"]
+                
+                usersRef.setValue(newUser)
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {

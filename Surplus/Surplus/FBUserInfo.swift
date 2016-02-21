5c1d7c83599a5c14908f1e965a1f3e9f0a0d0a24
Daniel Lee <cedanlee@gmail.com>
2016-02-21 01:17:16 -0800

    Adds fb user info

---
+++ Surplus/Surplus/FBUserInfo.swift
@@ -0,0 +1,19 @@
+//
+//  FBUserInfo.swift
+//  Surplus
+//
+//  Created by Daniel Lee on 2/21/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import Foundation
+import FBSDKLoginKit
+
+class FBUserInfo {
+    static var name: String?
+    static var id: String?
+    
+    class func isLoggedIn() -> Bool {
+        
+    }
+}

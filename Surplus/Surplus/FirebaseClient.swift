96fda20d620963f395e4bd490904024cc7067538
Daniel Lee <cedanlee@gmail.com>
2016-02-21 04:50:15 -0800

    Adds new firebaseclient

---
+++ Surplus/Surplus/FirebaseClient.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseClient.swift
+//  Surplus
+//
+//  Created by Daniel Lee on 2/21/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseClient {
+    
+}

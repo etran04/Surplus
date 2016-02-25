d5cbdf1c530e3e000e729877e7a7d990190fe66b
Eric Tran <erictritran@yahoo.com>
2016-02-24 19:06:10 -0800

    Cancel and done buttons navigate back to transactions

--- Surplus/Surplus/InputChargeVC.swift
+++ Surplus/Surplus/InputChargeVC.swift
@@ -21,15 +21,17 @@ class InputChargeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelPressed(sender: UIBarButtonItem) {
+        if let navController = self.navigationController {
+            navController.popViewControllerAnimated(true)
+        }
     }
-    */
+
+    @IBAction func donePressed(sender: UIBarButtonItem) {
+        if let navController = self.navigationController {
+            navController.popViewControllerAnimated(true)
+        }
+    }
+    
 
 }

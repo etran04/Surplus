6e12e0784ca66193e3d58785ff32c937f6d23c53
Daniel Lee <cedanlee@gmail.com>
2016-02-27 15:29:50 -0800

    Adds tabbarcontroller for monitoring notifications

---
+++ Surplus/Surplus/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  Surplus
+//
+//  Created by Daniel Lee on 2/27/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import Cocoa
+
+class MainTabBarController: UITabBarController {
+
+}

5f3e77f80992f23181a4e0f3f05a56a76a220c16
Daniel Lee <cedanlee@gmail.com>
2016-03-02 19:20:34 -0800

    Sets up table view controller for chatrooms

---
+++ Surplus/Surplus/MessagesTVC.swift
@@ -0,0 +1,13 @@
+//
+//  MessagesTVC.swift
+//  Surplus
+//
+//  Created by Daniel Lee on 3/2/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import UIKit
+
+class MessagesTVC: UITableViewController {
+
+}

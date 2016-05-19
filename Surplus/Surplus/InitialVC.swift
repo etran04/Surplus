9bda90f9b483fb1a61f993096f316e0b5a1ca96f
Daniel Lee <cedanlee@gmail.com>
2016-05-18 23:20:39 -0700

    Adds an initial screen for plus dollars

---
+++ Surplus/Surplus/InitialVC.swift
@@ -0,0 +1,25 @@
+//
+//  InitialVC.swift
+//  Surplus
+//
+//  Created by Daniel Lee on 5/18/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    @IBAction func yesPressed(sender: AnyObject) {
+    }
+    @IBAction func noPressed(sender: AnyObject) {
+    }
+}
+

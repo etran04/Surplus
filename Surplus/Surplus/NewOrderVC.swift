d5233f5d4594491013da36233fb7f7395cdc6e7f
Eric Tran <erictritran@yahoo.com>
2016-05-24 23:24:48 -0700

    Julian's cafe added to list of locations

--- Surplus/Surplus/NewOrderVC.swift
+++ Surplus/Surplus/NewOrderVC.swift
@@ -49,7 +49,7 @@ class NewOrderVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
         // Sets up scroll picker cell for locations
         let locationPickerCell = ScrollPickerCell(style: .Default, reuseIdentifier: nil)
-        self.locationChoices = ["The Avenue", "VG Cafe", "Starbucks UU", "Starbucks Campus Market", "Campus Market", "Village Market", "19 Metro Station", "Sandwich Factory", "Ciao!", "Red Radish", "Yogurt Creations", "Tacos To-Go Grill", "Jamba Juice", "Einstein Bro Bagels", "Subway"]
+        self.locationChoices = ["The Avenue", "VG Cafe", "Starbucks UU", "Starbucks Campus Market", "Campus Market", "Village Market", "19 Metro Station", "Sandwich Factory", "Ciao!", "Red Radish", "Yogurt Creations", "Tacos To-Go Grill", "Jamba Juice", "Einstein Bro Bagels", "Subway", "Julian's Café"]
         locationPickerCell.setChoices(self.locationChoices)
         
         let discountPickerCell = ScrollPickerCell(style: .Default, reuseIdentifier: nil)
